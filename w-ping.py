4c656260a8c248e15ede228e6db0cd18b92cd673
Wandering <wandering_997@sina.com>
2016-11-02 17:27:28 +0800

    add short name of some options

--- w-ping.py
+++ w-ping.py
@@ -402,6 +402,7 @@ Log format:
 Example:
 
   %s --ip 192.168.0.1,192.168.0.2
+  %s -f ip.test -d /tmp/test/ -i 0.1 -t 5
   %s --ipfile ip.test --datadir /tmp/test/ --interval 0.1 --timeout 5
   ''' % (sys.argv[0], sys.argv[0])
         )
